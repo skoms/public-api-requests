54cf49c0d64f34f52d343e6aadfdb550944e94b9
skoms <45171368+skoms@users.noreply.github.com>
2021-01-18 19:31:57 +0100

    add 'createModalWindow' function and add new classes to modal window

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,20 @@
  * Fetches data from the API specified in the function and parses it to JSON,
  * as well as throwing an error message if something went wrong
  */
-function fetchData() {
     fetch('https://randomuser.me/api/?results=12')
-        .then( response => response.json() )
-        .then( data => generateCards(data.results) )
-}
+        .then( response => response.json())
+        .then( data => {
+            generateCards(data.results);
+            createModalWindow(data.results);
+        })
 
-fetchData();
-/* 
-    <div class="card">
-        <div class="card-img-container">
-            <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-        </div>
-        <div class="card-info-container">
-            <h3 id="name" class="card-name cap">first last</h3>
-            <p class="card-text">email</p>
-            <p class="card-text cap">city, state</p>
-        </div>
-    </div> 
-*/
 
 function generateCards(employees) {
     const gallery = document.getElementById('gallery');
     for (let i = 0; i < employees.length; i++) {
         const employee = employees[i];
         gallery.insertAdjacentHTML('beforeend', `
-        <div class="card">
+        <div class="card card-${i}">
             <div class="card-img-container">
                 <img class="card-img" src="${employee.picture.large}" alt="profile picture">
             </div>
@@ -41,6 +29,32 @@ function generateCards(employees) {
     }
 }
 
-function cardEventHandler() {
-    
+function createModalWindow( employees ) {
+    const body = document.body;
+    for (let i = 0; i < employees.length; i++) {
+        const employee = employees[i];
+        body.insertAdjacentHTML('beforeend', `
+            <div class="modal-container modal-${i} hide">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                        <p class="modal-text">${employee.email}</p>
+                        <p class="modal-text cap">${employee.location.city}</p>
+                        <hr>
+                        <p class="modal-text">${employee.cell}</p>
+                        <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                        <p class="modal-text">Birthday: ${employee.dob.date}</p>
+                    </div>
+                </div>
+
+                <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                </div>
+            </div>
+        `);
+        console.log(`modal-${i} made`);
+    }
 }
